de2f63588d038b933da3de2b784a8132cb78a37e
Archaengel <god11341258@gmail.com>
2019-03-11 19:50:44 -0700

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
-module: {
-  rules: [
-    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-  ]
+const path = require("path");
+module.exports = {
+  entry: "./server.js",
+  output: {
+    path: path.join(__dirname, "./dist/"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    ]
+  }
 }
