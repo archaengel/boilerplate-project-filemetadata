4efd14a3f17da597127d9215491abb797a569fed
Archaengel <god11341258@gmail.com>
2019-03-11 19:54:29 -0700

    Change api, using nodemon

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/hello', (req, res, next) => {
-  res.send("<h1>HELLO</h1>")
+  res.send("<h1>HELLO Yo</h1>")
 })
 
 module.exports = router
