5699dccaaebc049f52685358b16259ee87e98149
Archaengel <god11341258@gmail.com>
2019-03-13 10:57:03 -0700

    Added server control to prevent server from starting when being tested

--- test/test.test.js
+++ test/test.test.js
@@ -1,3 +1,33 @@
 const chai = require("chai")
-const assert = chai.assert
+const expect = chai.expect
+const Maybe = require("folktale/maybe")
+const { Just, Nothing } = Maybe
+
+const {
+  howFly,
+  startServerIfCommandline
+} = require("../server.js")
+
+describe("server.js", () => {
+  describe("howFly when called", () => {
+    it("should return how fly", () => {
+      expect(howFly()).to.equal("soo fly")
+    })
+  })
+
+  describe("startServerIfCommandline when called", () => {
+    const mainStub = {}
+    const appStub = { listen: () => "net.Server" }
+    it("should return a net.Server if commandline", () => {
+      const result = startServerIfCommandline(mainStub)(mainStub)(appStub)(3000)
+        .getOrElse("cowface")
+      expect(result).to.equal("net.Server")
+    })
+
+    it("should return nothing if required", () => {
+      const result = startServerIfCommandline(mainStub)({})(appStub)(3000)
+      expect(result).to.not.equal("net.Server")
+    })
+  })
+})
 
